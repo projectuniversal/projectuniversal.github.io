039355d9816db9391a505661e22c22c6450c7223
Nyan cat <a0939712328@gmail.com>
2019-08-19 01:06:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ getElement = document.getElementById.bind(document)
 
 let gameLoopIntervalId = 0
 let player = getDefaultPlayer()
-let prologueAtom = new Decimal(1)
+let prologueAtom = new Decimal("9e89")
+let prologueGenActivated = false
 let storyId = 0
 let storyTexts = ["story text","story text 2","story text 3","this should not display"]
+
 getElement("storynext").onclick = function() {
     switch (storyId) {
         case 0:
@@ -20,6 +22,11 @@ getElement("storynext").onclick = function() {
           return;
     }
     getElement("introstory").innerHTML = storyTexts[storyId]
+}
+
+getElement("activategen").onclick = function() {
+    getElement("activategen").innerHTML = "ACTIVATED"
+    prologueGenActivated = true
 }
 
 function startInterval() {
@@ -34,7 +41,7 @@ function startGame() {
 function gameLoop(diff) { // 1 diff = 0.001 seconds
   var thisUpdate = new Date().getTime()
   if (typeof diff === 'undefined') var diff = Math.min(thisUpdate - player.lastUpdate, 21600000);
-  prologueAtom = prologueAtom.times(diff)
+  if (prologueGenActivated) prologueAtom = prologueAtom.plus(new Decimal("1e88").times(diff/1000))
   getElement("atomcount").innerHTML = shortenMoney(prologueAtom)
   player.lastUpdate = thisUpdate
 }
