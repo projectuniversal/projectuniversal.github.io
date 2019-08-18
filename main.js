7341a9744ade3531ea59eaa91fa33118dbe95ef5
Nyan cat <a0939712328@gmail.com>
2019-08-18 23:52:24 +0800

    Update main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+function getDefaultPlayer() {
+    return {
+        // yep, nothing
+    }
+}
+getElement = document.getElementById // will work...?
+
+let player = getDefaultPlayer()
+let storyId = 0
+let storyTexts = ["story text","story text 2","story text 3"]
+getElement("storynext").onclick = function() {
+    switch (storyId) {
+        case 0:
+        case 1:
+          storyId++
+    }
+    getElement("introstory").innerHTML = storyTexts[storyId]
+}
