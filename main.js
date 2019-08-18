e45cb15f56a6913ad60969dd68180e542349ab1c
Nyan cat <a0939712328@gmail.com>
2019-08-19 00:45:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 function getDefaultPlayer() {
     return {
-        // yep, nothing
+        lastUpdate: new Date().getTime()
     }
 }
-getElement = document.getElementById.bind(document) // will work...?
+getElement = document.getElementById.bind(document)
 
+let gameLoopIntervalId = 0
 let player = getDefaultPlayer()
-let prolodgeAtom = 9e89 // WHERE IS YOUR BREAK INFINITY
+let prologueAtom = new Decimal(9e89)
 let storyId = 0
 let storyTexts = ["story text","story text 2","story text 3","this should not display"]
 getElement("storynext").onclick = function() {
@@ -19,4 +20,20 @@ getElement("storynext").onclick = function() {
           return;
     }
     getElement("introstory").innerHTML = storyTexts[storyId]
+}
+
+function startInterval() {
+  gameLoopIntervalId = setInterval(gameLoop, 10)
+}
+
+function startGame() {
+  startInterval()
+  // There will be other thing here later
+}
+
+function gameLoop(diff) { // 1 diff = 0.001 seconds
+  var thisUpdate = new Date().getTime()
+  if (typeof diff === 'undefined') var diff = Math.min(thisUpdate - player.lastUpdate, 21600000);
+  // getElement("atomcount").innerHTML = shortenMoney(prologueAtom)
+  player.lastUpdate = thisUpdate
 }
