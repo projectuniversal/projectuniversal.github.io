95161652c40d1c4288454fa9f319a093ff654b4a
Nyan cat <a0939712328@gmail.com>
2019-08-19 00:00:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ getElement = document.getElementById.bind(document) // will work...?
 
 let player = getDefaultPlayer()
 let storyId = 0
-let storyTexts = ["story text","story text 2","story text 3"]
+let storyTexts = ["story text","story text 2","story text 3","this should not display"]
 getElement("storynext").onclick = function() {
     switch (storyId) {
         case 0:
