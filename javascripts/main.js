809ca4219d5d1355eb5f7e7e2e4f9a1f7b69de12
e666666 <a0939712328@gmail.com>
2019-08-19 13:04:29 +0800

    Code cleanup, prologue exit

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,9 @@
 function getDefaultPlayer() {
     return {
-        lastUpdate: new Date().getTime()
+        lastUpdate: new Date().getTime(),
+        inPrologue: true,
+        atom: new Decimal(0),
+        storyId: 0
     }
 }
 getElement = document.getElementById.bind(document)
@@ -9,15 +12,14 @@ let gameLoopIntervalId = 0
 let player = getDefaultPlayer()
 let prologueAtom = new Decimal("9e79")
 let prologueGenActivated = false
-let storyId = 0
-let storyTexts = ["Intro speak","Tells player to turn on gen","Explosion"]
+let storyTexts = ["Intro speak","Tells player to turn on gen","Explosion in 5 secs","The Beginning"]
 
 getElement("storynext").onclick = function() {
-    storyId = Math.min(1,storyId+1)
+    player.storyId = Math.min(1,player.storyId+1)
 }
 
 getElement("activategen").onclick = function() {
-    if (storyId < 1) return;
+    if (player.storyId < 1) return;
     getElement("activategen").innerHTML = "ACTIVATED"
     prologueGenActivated = true
 }
@@ -31,13 +33,30 @@ function startGame() {
   // There will be other thing here later
 }
 
+function endPrologue() {
+  player.inPrologue = false
+  player.storyId = Math.min(3,player.storyId+1)
+}
+
+function updateElement(id,text) {
+  getElement(id).innerHTML = text
+}
+
+function decideElementDisplay(id,bool) {
+  getElement(id).style.display = bool?"":"none"
+}
+
 function gameLoop(diff) { // 1 diff = 0.001 seconds
   var thisUpdate = new Date().getTime()
   if (typeof diff === 'undefined') var diff = Math.min(thisUpdate - player.lastUpdate, 21600000);
-  if (prologueGenActivated) prologueAtom = prologueAtom.plus(new Decimal("1e78").times(diff/1000))
-  if (prologueAtom.gte(new Decimal("1e80"))) storyId = 2
-  getElement("atomcount").innerHTML = shortenMoney(prologueAtom)
-  getElement("introstory").innerHTML = storyTexts[storyId]
-  getElement("storynext").style.display = storyId<1?"":"none"
+  if (player.inPrologue && prologueGenActivated) prologueAtom = prologueAtom.plus(new Decimal("1e78").times(diff/1000))
+  if (player.storyId == 1 && prologueAtom.gte(new Decimal("1e80"))) {
+    player.storyId = 2
+    setTimeout(endPrologue,5000)
+  }
+  updateElement("atomcount", shortenMoney(player.inPrologue?prologueAtom:player.atom))
+  updateElement("introstory", storyTexts[player.storyId])
+  decideElementDisplay("genContainer", player.inPrologue)
+  decideElementDisplay("storynext", player.storyId<1)
   player.lastUpdate = thisUpdate
 }
