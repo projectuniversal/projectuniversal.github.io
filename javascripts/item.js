048b31425bfdf0cf81beae478c9b95615f7fce5c
e666666 <a0939712328@gmail.com>
2019-10-20 16:14:14 +0800

    Upgrade type display

--- javascripts/item.js
+++ javascripts/item.js
@@ -1,9 +1,28 @@
 var existingTableNames = ["building","upgrade","research","discover"]
 var tableCellAmounts = {
   building: 4,
-  upgrade: 5,
+  upgrade: 6,
   research: 5,
   discover: 5
+}
+var tableDisplayCellIndex = {
+  effectDisplay: {
+    building: 1,
+    upgrade: 3,
+    research: 2,
+    discover: 2
+  },
+  cost: {
+    building: 2,
+    upgrade: 4,
+    research: 3,
+    discover: 3
+  },
+  desc: {
+    upgrade: 2,
+    research: 1,
+    discover: 1
+  }
 }
 var onclickCodeFunc = {
   building: (id) => `buyItem(${id}, 'building')`,
@@ -83,10 +102,11 @@ function updateItemTable(type) {
         decideElementDisplay(tr, showThisItem)
         if (showThisItem) {
           tr.cells[0].innerHTML = `${displayNames[type][id]} ${player.itemAmounts[type][id].gt(0)?`(${foo[type]} ${shortenMoney(player.itemAmounts[type][id])})`:""}`
-          if (type != "building") tr.cells[1].innerHTML = itemDescs[type][id]
-          tr.cells[type == "building"?1:2].innerHTML = itemEffectDisplayFunc[type](id)
-          tr.cells[type == "building"?2:3].innerHTML = `${shortenMoney(Decimal.ceil(player.itemCosts[type][id]))} ${itemCostCurrencyNameFunc[type](id)}s`
-          let buyButton = tr.cells[type == "building"?3:4].childNodes[0]
+          if (type != "building") tr.cells[tableDisplayCellIndex.desc[type]].innerHTML = itemDescs[type][id]
+          if (type == "upgrade") tr.cells[1].innerHTML = upgradeTypes[id]=="c"?"Construction":"Experiement"
+          tr.cells[tableDisplayCellIndex.effectDisplay[type]].innerHTML = itemEffectDisplayFunc[type](id)
+          tr.cells[tableDisplayCellIndex.cost[type]].innerHTML = `${shortenMoney(Decimal.ceil(player.itemCosts[type][id]))} ${itemCostCurrencyNameFunc[type](id)}s`
+          let buyButton = tr.cells[tr.cells.length - 1].childNodes[0]
           let availability = itemAvailabilityFunc[type](id)
           let displayTexts = itemDisplayTexts[type]
           buyButton.innerHTML = displayTexts[availability]
