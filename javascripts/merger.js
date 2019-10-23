8cf8425103b4c924be3726336da7ea8b089f073d
e666666 <a0939712328@gmail.com>
2019-10-23 18:56:54 +0800

    WORKING MOLECULE MERGER
    as always, easier than i expected.

--- javascripts/merger.js
+++ javascripts/merger.js
@@ -1,3 +1,10 @@
 function updateMergerDescs() {
   updateElement("atomMergerDesc", `Atom Merger: ${player.particleAtomRatio} Particles -> 1 Atom, ${shorten(player.mergePower)} bulks max, 1s interval`)
 }
+
+function moleculeMergerActivate() {
+  if (player.atom.lt(player.atomMoleculeRatio) || player.moleculeMergerOn) return false
+  player.moleculeMergerOn = true
+  player.moleculeMergerTime = 0
+  return true
+}
